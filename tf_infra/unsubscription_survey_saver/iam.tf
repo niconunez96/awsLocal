9e8f82c248287daa047bf0b4cd9b5b5506990da0
niconunez96 <nicolas110996@gmail.com>
2023-07-17 00:02:11 -0300

    Add SNS topic for lambda

--- tf_infra/unsubscription_survey_saver/iam.tf
+++ tf_infra/unsubscription_survey_saver/iam.tf
@@ -15,3 +15,27 @@ resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
+
+
+data "aws_iam_policy_document" "lambda_sns_publish" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sns:Publish",
+    ]
+    resources = [aws_sns_topic.unsubscription_survey_submitted.arn]
+  }
+}
+
+resource "aws_iam_policy" "lambda_sns_publish" {
+  name        = "lambda_sns_publish"
+  path        = "/"
+  description = "IAM policy for publishing into unsubscription survey submitted"
+  policy      = data.aws_iam_policy_document.lambda_sns_publish.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_sns_publish.arn
+}
--- tf_infra/unsubscription_survey_saver/main.tf
+++ tf_infra/unsubscription_survey_saver/main.tf
@@ -1,3 +1,7 @@
+resource "aws_sns_topic" "unsubscription_survey_submitted" {
+  name = "unsubscription_survey_submitted"
+}
+
 data "archive_file" "lambda" {
   type        = "zip"
   source_file = "../lambdas/src/subscription_bought_publisher.py"
@@ -10,7 +14,7 @@ resource "aws_lambda_function" "test_function" {
   handler       = "subscription_bought_publisher.handler"
   filename      = "subscription_bought_publisher.zip"
   runtime       = "python3.9"
-  timeout = 15
+  timeout       = 15
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
